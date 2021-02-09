ff5da36accc2d0b1b03c23c1ad0d0ba048f6131a
svsannidhay <sannidhayvashalsv@gmail.com>
2021-02-09 21:00:36 +0530

    Some styling issues fixed

--- client/src/PostForm.js
+++ client/src/PostForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Grid.css';
 import './PostForm.css';
+import './Button.css';
 import axios from './axios';
 
 class PostForm extends React.Component {
@@ -33,29 +34,33 @@ class PostForm extends React.Component {
     return(
       <section className="postForm ">
       <div className="container">
-        <div className="input-form container col span-1-of-2">
+        <div className="input-form col span-1-of-2">
           <h3 className="postForm-text">Post a meme</h3>
           <form onSubmit = {this.onFormSubmit}>
             <input className ="input-field input-name" type="text"
               placeholder = "Name" value = {this.state.name} 
               name = "name"
               onChange = {this.onInputChange}
+              required
             />
             <input className ="input-field input-caption" type="text"
               placeholder = "Caption" value = {this.state.caption}
               name = "caption"
               onChange = {this.onInputChange}
+              required
             />
             <input className ="input-field filed input-url" type="text"
               placeholder = "Meme Url" value = {this.state.imgUrl}
               name ="imgUrl"
               onChange = {this.onInputChange}
+              required
             />
-            <button className="input-button" 
+            <button className="btn" 
             type="submit">Submit</button>
           </form>
         </div>
         <div className="col span-1-of-2">
+          
         </div>
         <div className="clearfix"></div>
       </div>
